770c5c3207fa29b3ff406b5669ec9a8f486ecd0c
Bailey Capuano <bcapuano@asu.edu>
2022-12-29 20:31:10 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_time.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import atheris
+import sys
+import fuzz_helpers
+import random
+
+with atheris.instrument_imports():
+    from pytime import pytime
+
+from pytime.exception import PyTimeException
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    test = fdp.ConsumeIntInRange(0, 2)
+    try:
+        if test == 0:
+            pytime.before(fdp.ConsumeRemainingString())
+        elif test == 1:
+            pytime.tomorrow(fdp.ConsumeRemainingString())
+        elif test == 2:
+            pytime.parse(fdp.ConsumeRemainingString())
+    except (PyTimeException, ValueError):
+        return -1
+    except (TypeError, IndexError):
+        if random.random() > 0.99:
+            raise
+        return -1
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
